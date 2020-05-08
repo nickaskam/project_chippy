7748e81aa555cb4da8205608d10143e5cad9844e
Nick Askam <asknickfilms@gmail.com>
2020-05-08 14:18:27 -0500

    import not working... connecting react

---
+++ project_chippy/frontend/src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Project Chippy</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
