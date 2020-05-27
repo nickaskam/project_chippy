4ada5dcb8340807eef59acec9c888c49ac14909c
Nick Askam <asknickfilms@gmail.com>
2020-05-26 19:25:38 -0500

    adding comments

--- project_chippy/frontend/src/components/events/ShorthandList.js
+++ project_chippy/frontend/src/components/events/ShorthandList.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+//showing the event name in a unordered list
 function EventList(props) {
   const eventlist = props.events.map((event) => {
     return <RenderEvent event={event} key={event.id} />;
--- project_chippy/frontend/src/components/layout/Header.js
+++ project_chippy/frontend/src/components/layout/Header.js
@@ -8,15 +8,19 @@ export class Header extends Component {
         <nav>
           <ul>
             <li>
+              {/* Home Page */}
               <a href="#">Project Chippy</a>
             </li>
             <li>
+              {/* Add a new event */}
               <Link to="/form">Add New Event</Link>
             </li>
             <li>
+              {/* All of the events in detail */}
               <Link to="/events">Events List</Link>
             </li>
             <li>
+              {/* A list of the events only showing the name */}
               <Link to="/shortlist">Short List</Link>
             </li>
           </ul>
