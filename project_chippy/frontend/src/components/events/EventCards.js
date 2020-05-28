050f891a6519f02802fbc786a9cf6b390355f6ba
Nick Askam <asknickfilms@gmail.com>
2020-05-27 20:04:00 -0500

    added a component of event cards to break up events js and starting working on creating a sorting component

---
+++ project_chippy/frontend/src/components/events/EventCards.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+export class EventCards extends Component {
+  //show date and time from datetime field
+  dateFormatter = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  //show date only from datetime field
+  dateFormatterDateOnly = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  });
+
+  //show time only from datetime field
+  dateFormatterTimeOnly = new Intl.DateTimeFormat("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  render() {
+    return (
+      <div>
+        {/* Overview of event */}
+        <h4>
+          {this.props.event.name} - {this.props.event.category} -{" "}
+          {this.dateFormatterDateOnly.format(
+            Date.parse(this.props.event.start_time)
+          )}
+        </h4>
+        {/* Start to end times only */}
+        <p>
+          {this.dateFormatterTimeOnly.format(
+            Date.parse(this.props.event.start_time)
+          )}{" "}
+          -{" "}
+          {this.dateFormatterTimeOnly.format(
+            Date.parse(this.props.event.end_time)
+          )}
+        </p>
+        {/* Shows event description */}
+        <p>{this.props.event.description}</p>
+        {/* Shows if event is complete */}
+        <p>Complete? {this.props.event.complete}.</p>
+        {/* Shows when event was created */}
+        <p>
+          Created at:{" "}
+          {this.dateFormatter.format(Date.parse(this.props.event.created_at))}
+        </p>
+        {/* Delete Button */}
+        <div id="eventsDeleteButtonDiv">
+          {
+            <button
+              onClick={() => this.props.deleteEvent(event.id)}
+              className="deleteButton"
+            >
+              Delete
+            </button>
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EventCards;
--- project_chippy/frontend/src/components/events/Events.js
+++ project_chippy/frontend/src/components/events/Events.js
@@ -1,88 +1,95 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import { deleteEvent } from "../../actions/events";
 import Buttons from "./Buttons";
+import EventCards from "./EventCards";
 
 export class Events extends Component {
   constructor(props) {
     super(props);
+    this.sortByDateAsc = this.sortByDateAsc.bind(this);
+    this.sortByDateDesc = this.sortByDateDesc.bind(this);
   }
 
   static propTypes = {
     deleteEvent: PropTypes.func.isRequired,
   };
 
-  //show date and time from datetime field
-  dateFormatter = new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "short",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  sortByDateAsc() {
+    this.props.events.sort((a, b) => (a.start_time > b.start_time ? 1 : -1));
+  }
 
-  //show date only from datetime field
-  dateFormatterDateOnly = new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "short",
-    day: "2-digit",
-  });
-
-  //show time only from datetime field
-  dateFormatterTimeOnly = new Intl.DateTimeFormat("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  sortByDateDesc() {
+    this.props.events.sort((a, b) => (b.start_time > a.start_time ? 1 : -1));
+  }
 
   render() {
     return (
       <Fragment>
-        {<Buttons />}
+        <Buttons />
+        <SortButtons />
+        <button onClick={this.sortByDateAsc}>Asc Date</button>
+        <button onClick={this.sortByDateDesc}>Desc Date</button>
         <h2>Events</h2>
         <div className="grid">
           {this.props.events.map((event) => (
-            <div key={event.id}>
-              {/* Overview of event */}
-              <h4>
-                {event.name} - {event.category} -{" "}
-                {this.dateFormatterDateOnly.format(
-                  Date.parse(event.start_time)
-                )}
-              </h4>
-              {/* Start to end times only */}
-              <p>
-                {this.dateFormatterTimeOnly.format(
-                  Date.parse(event.start_time)
-                )}{" "}
-                -{" "}
-                {this.dateFormatterTimeOnly.format(Date.parse(event.end_time))}
-              </p>
-              {/* Shows event description */}
-              <p>{event.description}</p>
-              {/* Shows if event is complete */}
-              <p>Complete? {event.complete}.</p>
-              {/* Shows when event was created */}
-              <p>
-                Created at:{" "}
-                {this.dateFormatter.format(Date.parse(event.created_at))}
-              </p>
-              {/* Delete Button */}
-              <div id="eventsDeleteButtonDiv">
-                {
-                  <button
-                    onClick={() => this.props.deleteEvent(event.id)}
-                    className="deleteButton"
-                  >
-                    Delete
-                  </button>
-                }
-              </div>
-            </div>
+            <EventCards key={event.id} event={event} />
           ))}
         </div>
       </Fragment>
+    );
+  }
+}
+
+class SortButtons extends Component {
+  state = {
+    sortAscStartTime: false,
+    sortAscEventName: false,
+    fieldSort: "",
+  };
+
+  toggleAscendingStartTime() {
+    this.setState({
+      sortAscStartTime: !this.state.sortAscStartTime,
+    });
+  }
+
+  toggleAscendingEvent() {
+    this.setState({
+      sortAscEventName: !this.state.sortAscEventName,
+    });
+  }
+
+  toggleFieldSort = (field) => {
+    this.setState({
+      fieldSort: field,
+    });
+    if (field == "start_time") {
+      this.toggleAscendingStartTime();
+    } else if (field == "name") {
+      this.toggleAscendingEvent();
+    } else {
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleFieldSort.bind(this, "start_time")}>
+          Change sort by start time
+        </button>
+        <button onClick={this.toggleFieldSort.bind(this, "name")}>
+          Change sort by event name
+        </button>
+        <p>
+          Sort Ascending Start Time: {this.state.sortAscStartTime.toString()}
+        </p>
+        <p>
+          Sort Ascending Event Name: {this.state.sortAscEventName.toString()}
+        </p>
+        <p>Field Sort: {this.state.fieldSort}</p>
+      </div>
     );
   }
 }
