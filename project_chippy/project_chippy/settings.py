ff2e8aced74401a03fc2ef696e11eb8e485c5da5
Nick Askam <asknickfilms@gmail.com>
2020-06-11 16:50:20 -0500

    added database name

--- project_chippy/project_chippy/settings.py
+++ project_chippy/project_chippy/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'project_chippy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'projectChippyDB',
+        'NAME': 'projectchippydb',
         'USER': 'nick',
         'PASSWORD': 'ch1ppyp@55w0rD',
         'HOST': 'localhost',
