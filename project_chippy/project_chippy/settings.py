7f19f8ceb7630e775764906d8e4497b5595787ef
Nick Askam <asknickfilms@gmail.com>
2020-06-11 16:45:39 -0500

    added database and static urls

--- project_chippy/project_chippy/settings.py
+++ project_chippy/project_chippy/settings.py
@@ -86,11 +86,12 @@ WSGI_APPLICATION = 'project_chippy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'projectchippy',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projectChippyDB',
+        'USER': 'nick',
+        'PASSWORD': 'ch1ppyp@55w0rD',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -132,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 try:
     from local_settings import *
