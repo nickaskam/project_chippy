f0d91be966c37eb5e96069d8031b0c8b20f83e1b
Nick Askam <asknickfilms@gmail.com>
2020-06-11 19:29:22 -0500

    changing order of allowed hosts

--- project_chippy/project_chippy/settings.py
+++ project_chippy/project_chippy/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$25w5vw1t6g*0cv1%oy!eyo!w8)1)6muwnd6_-07)i1gt3^o*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['206.81.4.160', 'projectchippy.com', 'localhost']
+ALLOWED_HOSTS = ['projectchippy.com', '206.81.4.160', 'localhost']
 
 
 # Application definition
@@ -86,12 +86,11 @@ WSGI_APPLICATION = 'project_chippy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'projectchippydb',
-        'USER': 'nick',
-        'PASSWORD': 'ch1ppyp@55w0rD',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projectchippy',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
