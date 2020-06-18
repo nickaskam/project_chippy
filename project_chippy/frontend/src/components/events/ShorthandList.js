816cf9ee8eee352a89c8a0bc353c3e54015e3404
Nick Askam <asknickfilms@gmail.com>
2020-06-18 13:20:50 -0500

    minor fixes

--- project_chippy/frontend/src/components/events/ShorthandList.js
+++ project_chippy/frontend/src/components/events/ShorthandList.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import EventCards from "./EventCards";
 import EventFooter from "../layout/EventFooter";
 
