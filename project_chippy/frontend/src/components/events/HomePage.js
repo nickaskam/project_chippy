6cd10bd30eb9c9fa1d6ebdca9222331997895cb4
Nick Askam <asknickfilms@gmail.com>
2020-06-18 12:44:39 -0500

    adding in redirect to go to home page if you're logged in

--- project_chippy/frontend/src/components/events/HomePage.js
+++ project_chippy/frontend/src/components/events/HomePage.js
@@ -1,16 +1,33 @@
-import React, { Fragment } from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Redirect } from "react-router-dom";
 
-function HomePage() {
-  return (
-    <Fragment>
-      <h1>Project Chippy</h1>
-      <p>
-        Welcome to the Home Page of the app. Please use the links to click to
-        the assigned pages. Use this app as your way to track events and as a to
-        do list.
-      </p>
-    </Fragment>
-  );
+class HomePage extends Component {
+  static propTypes = {
+    isAuthenticated: PropTypes.bool,
+  };
+
+  render() {
+    if (this.props.isAuthenticated) {
+      return <Redirect to="/home" />;
+    }
+
+    return (
+      <div className="container">
+        <h1>Project Chippy</h1>
+        <p>
+          Welcome to the Home Page of the app. Please use the links to click to
+          the assigned pages. Use this app as your way to track events and as a
+          to do list.
+        </p>
+      </div>
+    );
+  }
 }
 
-export default HomePage;
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(HomePage);
